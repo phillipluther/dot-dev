849f2ed9831a5caaa79019176a54c7657fbf8dcd
Phillip Luther <dev@phillipluther.com>
2021-05-25 07:02:08 -0700

    Morningly; minor updates to building archive pages

--- scripts/build-archives.ts
+++ scripts/build-archives.ts
@@ -1,6 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 import nunjucks from 'nunjucks';
+import dashify from 'dashify';
 import getPostData, { PostData } from './get-post-data';
 import { TEMPLATES_DIR, DIST_DIR } from './constants';
 
@@ -15,32 +16,33 @@ async function buildArchives(): Promise<PostData[]> {
     });
     
     const allPostData = await getPostData();
-    const postsByTag: object = allPostData.reduce((byTag, postData) => {
+    const allTagData: object = allPostData.reduce((tagData, postData) => {
       const { tags } = postData;
 
       tags.forEach((tag) => {
-        // TODO" make friendly tag names (hyphenated)
-        // const friendlyTag = 
+        const dashifiedTag = dashify(tag);
 
-        if (!byTag[tag]) {
-          byTag[tag] = [];
+        if (!tagData[dashifiedTag]) {
+          tagData[dashifiedTag] = {
+            title: tag,
+            description: `Posts from The Principled Engineer about ${tag.toLowerCase()}`,
+            posts: [],
+          };
         }
 
-        byTag[tag].push(postData);
+        tagData[dashifiedTag].posts.push(postData);
       });
 
-      return byTag;
+      return tagData;
     }, {});
 
-    const promisedArchives = Promise.all(Object.keys(postsByTag).map(async (tag) => {
+    const promisedArchives = Promise.all(Object.keys(allTagData).map(async (tag) => {
       try {
-        const rendered = nunjucks.render('archive.njk', {
-          posts: postsByTag[tag],
-        });
+        const rendered = nunjucks.render('archive.njk', allTagData[tag]);
         const archiveDir = path.join(DIST_DIR, tag);
 
         await fs.mkdir(archiveDir, { recursive: true });
-        // await fs.writeFile(path.join(archiveDir, 'index.html'), rendered);
+        await fs.writeFile(path.join(archiveDir, 'index.html'), rendered);
       } catch (writeError) {
         console.error(writeError);
       }
