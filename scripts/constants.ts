d8f83bdf28473f7ecbc40f7101afcdb25dcef8dd
Phillip Luther <dev@phillipluther.com>
2021-05-26 21:14:05 -0700

    play with marked/prism

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -6,11 +6,18 @@ const POSTS_DIR = path.join(SRC_DIR, 'posts');
 const TEMPLATES_DIR = path.join(SRC_DIR, 'templates');
 const BASE_URL = 'https://principledengineer.com';
 
+// newer, WIP
+const POSTS_SRC_DIR = path.join(SRC_DIR, 'posts');
+const POSTS_DIST_DIR = path.join(DIST_DIR, 'posts');
+
 export {
   BASE_URL,
   SRC_DIR,
   DIST_DIR,
   POSTS_DIR,
   TEMPLATES_DIR,
+  // newer, WIP
+  POSTS_SRC_DIR,
+  POSTS_DIST_DIR,
 }
 
