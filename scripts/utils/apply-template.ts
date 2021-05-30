b37e06fcb66d46ff9c5a625958e2db0ec08bcbfd
Phillip Luther <dev@phillipluther.com>
2021-05-29 21:18:09 -0700

    Extended date handling; inclusion of tag links in archives

--- scripts/utils/apply-template.ts
+++ scripts/utils/apply-template.ts
@@ -1,5 +1,6 @@
 import * as nunjucks from 'nunjucks';
 import dayjs from 'dayjs';
+import dashify from 'dashify';
 import { PostMetadata } from './get-post-data';
 import { TEMPLATES_SRC_DIR, DATE_FORMAT } from './constants';
 
@@ -7,7 +8,9 @@ const nunjucksEnv = nunjucks.configure(TEMPLATES_SRC_DIR, {
   autoescape: true,
 });
 
-nunjucksEnv.addFilter('date', (date: string): string => dayjs(date).format(DATE_FORMAT));
+nunjucksEnv.addFilter('friendlyDate', (date: string): string => dayjs(date).format(DATE_FORMAT));
+nunjucksEnv.addFilter('simpleDate', (date: string): string => dayjs(date).format('YYYY-MM-DD'));
+nunjucksEnv.addFilter('tag', (tag: string): string => dashify(tag));
 
 function applyTemplate(templatePath: string, data: PostMetadata): string {
   return nunjucksEnv.render(templatePath, data);
